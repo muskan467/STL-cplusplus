cde8b09bb2a44d7fac8e1cc7ae02849802f9d721
Muskan Ali Qureshi <qmuskan467@gmail.com>
2023-01-16 20:48:40 +0530

    Update explainVector.cpp

--- explainVector.cpp
+++ explainVector.cpp
@@ -8,5 +8,10 @@ int main()
   v.push_back(1);
   v.emplace_back(2); // similar to push_back but faster, as it does not makes copies of old vectors
   
+  vector<pair<int, int>> vec;
+  
+  vec.push_back({1, 3});
+  vec.emplace_back(4, 5); // already assumes to be a pair
+  
   return 0;
 }
